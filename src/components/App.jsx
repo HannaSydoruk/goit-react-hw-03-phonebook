ab408b29f6681ce890b4cc276b8e0f3e5e4846a5
HannaSydoruk <hanna.sydoruk@gmail.com>
2023-12-18 10:05:48 +0200

    localStorage added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,20 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
   formSubmitHandler = data => {
     const hasDuplicates = this.state.contacts.some(
       contact => contact.name.toLowerCase() === data.name.toLocaleLowerCase()
